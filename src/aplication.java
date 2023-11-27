5ddc0eed86d6a002467eb64992a5971f26f96e72
clara02mc <maria.cvm172002@gmail.com>
2023-11-26 21:49:09 -0300

    att do projeto

--- src/aplication.java
+++ src/aplication.java
@@ -4,20 +4,27 @@ public class aplication {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		System.out.println("Bem vind a E-Books");
+
+		System.out.println("Bem vindo a E-Books");
 		System.out.print("Digite seu nome: ");
 		String name = sc.nextLine();
 		System.out.println("Ola, " + name );
 		
+		
 		System.out.print("Digite seu endereço: " );
-		int adress = sc.nextInt();
+		String address = sc.nextLine();
+		 
 		System.out.print("Digite sua idade: ");
-		String age = sc.nextLine();
-		System.out.print("Digite seu gênero (F/M): ");
+		String age =  sc.nextLine();
+		
+		
+		System.out.print("Digite seu gênero (F/M): " );
 		String gender = sc.nextLine();
+	
+//		System.out.println("Menu de opções:");
+//		System.out.println("1 - Criar Pedidos \n 2- Editar Dados \n 3- Voltar");
+//		
+//		System.out.println("Escolha uma opção:");
 		
 		
 		
